3b75e636625e80e1165288c2a2c4fdda41da7fd3
Alexis Morel <alexis.morel@payfit.com>
2025-12-22 02:01:15 +0100

    WIP

--- flake.nix
+++ flake.nix
@@ -1,15 +1,15 @@
 {
   description = "Home Manager configuration of reiwilde";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-    ];
+  #nixConfig = {
+  #  extra-substituters = [
+  #    "https://nix-community.cachix.org"
+  #  ];
 
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
+  #  extra-trusted-public-keys = [
+  #    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  #  ];
+  #};
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
